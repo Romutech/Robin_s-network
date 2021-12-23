e3701b58c6bdba9dbdb48b7f66f72d0f60adee42
Robin Muller <robinmuller75009@icloud.com>
2021-12-23 23:37:40 +0100

    feat(user_profile/models): creation of profile model #4

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    first_name = models.CharField(max_length=50, null=True)
+    last_name = models.CharField(max_length=50, null=True)
+    date_of_birth = models.DateField(null=True)
+    profession = models.CharField(max_length=50, null=True)
+    residence = models.CharField(max_length=50, null=True)
