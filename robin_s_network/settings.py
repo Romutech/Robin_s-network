b12df3701479d730c294f8a185bafdbd8dee82d3
Robin Muller <robinmuller75009@icloud.com>
2021-12-23 22:45:36 +0100

    chore: install and configuration of postgres #7

--- robin_s_network/settings.py
+++ robin_s_network/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'robin_s_network.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'robin_s_network',
+        'USER': 'robinmuller',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
