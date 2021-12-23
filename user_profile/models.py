92e408588ff9c6684a31aa7b52789c7f0b26e178
Robin Muller <robinmuller75009@icloud.com>
2021-12-24 00:25:24 +0100

    feature: add user relation on the model and re-generate all user_profile migrations #4

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
     first_name = models.CharField(max_length=50, null=True)
     last_name = models.CharField(max_length=50, null=True)
     date_of_birth = models.DateField(null=True)
